73b40f377d00965fb30220e785e9fa0edef6fea9
Osama Khawar <osama.khawar@gomotive.com>
2022-12-29 00:04:35 +0500

    Fixing issues with html, css in the system

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,3 +4,16 @@ hamMenu.addEventListener('click', () => {
 	hamMenu.classList.toggle('active');
 	offScreenMenu.classList.toggle('active');
 });
+
+/* Adding class to navigation bar on scroll */
+$(window).scroll(function() {
+    var nav = $('#main-nav');
+    var top = 200;
+    if ($(window).scrollTop() >= top) {
+
+        nav.addClass('illuminated-nav');
+
+    } else {
+        nav.removeClass('illuminated-nav');
+    }
+});
