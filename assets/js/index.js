4d0c7fd79feb81a2a0bba3eda634ee0d0508a8cb
SafdarThaheem <104439594+SafdarThaheem@users.noreply.github.com>
2023-01-01 20:04:44 +0500

    awards section html,css and js completed

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,3 +17,24 @@ $(window).scroll(function() {
         nav.removeClass('illuminated-nav');
     }
 });
+
+// awards section slider
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:false,
+    autoplay:true,
+    autoplayTimeout:60000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:1
+        }
+    }
+})
